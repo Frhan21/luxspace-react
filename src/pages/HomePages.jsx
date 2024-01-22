d819702e3c395ffd5fdf3274ac1a09c73b565c6d
unknown <2010441005_m@student.unand.ac.id>
2024-01-22 16:38:53 +0700

    Custom Hooks

--- src/pages/HomePages.jsx
+++ src/pages/HomePages.jsx
@@ -1,21 +1,25 @@
-import Header from '../parts/Header'
-import Hero from '../parts/Hero'
-import JustArrived from '../parts/Homepage/JustArrived'
-import BrowserRoom from '../parts/Homepage/BrowserRoom'
-import Sitemaps from '../parts/Homepage/Sitemaps'
-import Client from '../parts/Client'
-import Footer from '../parts/Footer'
+import Header from "../parts/Header";
+import Hero from "../parts/Homepage/Hero";
+import JustArrived from "../parts/Homepage/JustArrived";
+import BrowserRoom from "../parts/Homepage/BrowserRoom";
+import Sitemaps from "../parts/Homepage/Sitemaps";
+import Client from "../parts/Client";
+import Footer from "../parts/Footer";
+import useScrollAnchor from "../helper/hooks/useScrollAnchor";
+import useModalDOM from "../helper/hooks/useModalDOM";
 
 export default function HomePages() {
+  useScrollAnchor();
+  useModalDOM();
   return (
     <>
-      <Header theme="white" position="absolute"/>
-      <Hero/>
-      <BrowserRoom/>
-      <JustArrived/>
-      <Client/>
-      <Sitemaps/>
-      <Footer/>
+      <Header theme="white" position="absolute" />
+      <Hero />
+      <BrowserRoom />
+      <JustArrived />
+      <Client />
+      <Sitemaps />
+      <Footer />
     </>
-  )
+  );
 }
